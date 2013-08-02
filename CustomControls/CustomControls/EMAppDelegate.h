97418d86d962c1a26eb12e403235643289e1daad
Eric McConkie <mcconkiee@gmail.com>
2013-08-01 22:34:25 -0700

    custom tab bar

--- CustomControls/CustomControls/EMAppDelegate.h
+++ CustomControls/CustomControls/EMAppDelegate.h
@@ -14,6 +14,6 @@
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) EMViewController *viewController;
+@property (strong, nonatomic) UITabBarController *viewController;
 
 @end
--- CustomControls/CustomControls/EMAppDelegate.m
+++ CustomControls/CustomControls/EMAppDelegate.m
@@ -16,8 +16,43 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.viewController = [[EMViewController alloc] initWithNibName:@"EMViewController" bundle:nil];
+    self.viewController = [[UITabBarController alloc] init];
     self.window.rootViewController = self.viewController;
+    
+    
+    [self.viewController setViewControllers:[NSArray arrayWithObjects:
+                                             [[EMViewController alloc] init],
+                                             [[UIViewController alloc] init],
+                                             [[UIViewController alloc] init],
+                                             [[UIViewController alloc] init]
+                                             , nil]];
+    UIImage *selectedImage0 = [UIImage imageNamed:@"tab_0_on"];
+    UIImage *unselectedImage0 = [UIImage imageNamed:@"tab_0_off"];
+    
+    UIImage *selectedImage1 = [UIImage imageNamed:@"tab_1_on"];
+    UIImage *unselectedImage1 = [UIImage imageNamed:@"tab_1_off"];
+    
+    UIImage *selectedImage2 = [UIImage imageNamed:@"tab_2_on"];
+    UIImage *unselectedImage2 = [UIImage imageNamed:@"tab_2_off"];
+    
+    UIImage *selectedImage3 = [UIImage imageNamed:@"tab_3_on"];
+    UIImage *unselectedImage3 = [UIImage imageNamed:@"tab_3_off"];
+    
+    
+    UITabBar *tabBar = self.viewController.tabBar;
+    UITabBarItem *item0 = [tabBar.items objectAtIndex:0];
+    UITabBarItem *item1 = [tabBar.items objectAtIndex:1];
+    UITabBarItem *item2 = [tabBar.items objectAtIndex:2];
+    UITabBarItem *item3 = [tabBar.items objectAtIndex:3];
+    
+    
+    [item0 setFinishedSelectedImage:selectedImage0 withFinishedUnselectedImage:unselectedImage0];
+    [item1 setFinishedSelectedImage:selectedImage1 withFinishedUnselectedImage:unselectedImage1];
+    [item2 setFinishedSelectedImage:selectedImage2 withFinishedUnselectedImage:unselectedImage2];
+    [item3 setFinishedSelectedImage:selectedImage3 withFinishedUnselectedImage:unselectedImage3];
+    
+    
+    
     [self.window makeKeyAndVisible];
     return YES;
 }
